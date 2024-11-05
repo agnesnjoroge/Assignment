0d3b2c8791dde422e9c63f8276fad3ecdb0eca19
Agnes Njoroge <anjoroge@greencom.co.ke>
2024-11-05 14:11:38 +0300

    first commit

---
+++ Core/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerService _customerService;
+
+        public CustomerController(CustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var allCustomers = await _customerService.GetCustomersAsync();
+            return Ok(allCustomers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerById(string id)
+        {
+            var customer = await _customerService.GetCustomerByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+    }
+}
---
+++ Core/Program.cs
@@ -0,0 +1,30 @@
+using Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Register CustomerService with HttpClient and default credentials.
+builder.Services.AddHttpClient<CustomerService>()
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        UseDefaultCredentials = true,
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
